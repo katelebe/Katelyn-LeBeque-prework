9f9bec4b67c54ed76717e87b94ba0061950aa8a9
Katelyn LeBeque <kate.lebeque@gmail.com>
2020-06-08 14:28:29 -0500

    more basic mechanics

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -27,36 +27,51 @@ var guessedLetters = [];
 
 // checks if letter hase been guessed yet, if not, throws into evaluate function
 function guess(letter) {
-    console.log(letter)
     if (guessedLetters.indexOf(letter) === -1) {
         guessedLetters.push(letter);
         evaluateGuess(letter);
     }
 }
 
-//replace element in a string?
-//String.prototype.replaceAt = function(index, replacement) {
-//    return this.substring(0, index) + replacement + this.substring(index + 1)
-//}
+let badGuess = [];
+// initial guesses
+numGuesses = 10;
+guesses.innerText = numGuesses;
 
-// decides whether letter is in word or not
+function guessUpdate() {
+    let guesses = document.querySelector('#guesses');
+    guesses.innerText = numGuesses;
+    if (numGuesses === 0) {
+        randomWord();
+        badGuess = [];
+        let wrong = document.querySelector('#incorrect');
+        wrong.innerText = badGuess;
+        numGuesses = 10;
+    }
+}
+
+// decides whether letter is in word or not, runs each time a key is pressed that wasn't guessed before
 function evaluateGuess(letter) {
     var positions = [];
     for (let i = 0; i < window.puzzle.length; i++) {
         if (window.puzzle[i] === letter) {
             positions.push(i);
-        }
+        } 
     };
 
-    console.log(positions);
-    
-    console.log(unsolvedSpaces)
-    
+    if (positions.length === 0) {
+        badGuess.push(letter);
+        let wrong = document.querySelector('#incorrect');
+        wrong.innerText = badGuess.join(', ');
+        numGuesses--;
+        guessUpdate();
+    }
+
     // *2 because in my string I have a space after each '_' jk it's still not perfect
     for (let i = 0; i < positions.length; i++) {
         unsolvedSpaces = unsolvedSpaces.substring(0, (2*positions[i])) + letter + unsolvedSpaces.substring(2*(positions[i])+1, unsolvedSpaces.length);
-        console.log(unsolvedSpaces);
         let solving = document.querySelector('#word');
         solving.innerText = unsolvedSpaces;
     };
 }
+
